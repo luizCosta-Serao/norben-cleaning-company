eea60fa8a5238d30f1b1bf8f1ea74f66a60fcca1
Luiz Serão <luiz.serao@hotmail.com>
2024-05-29 09:03:54 -0300

    novo link de redirecionamento para whatsapp

--- frontend/src/components/Hero/Hero.tsx
+++ frontend/src/components/Hero/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <section id='hero' className={styles.hero}>
       <h1>GET CLEANED</h1>
       <p>with CC Commercial Cleaners</p>
-      <a target='_blank' href="https://w.app/D9moOk">Get appointment now</a>
+      <a target='_blank' href="https://api.whatsapp.com/send?phone=5544988099490&text=Em%20que%20posso%20te%20ajudar?">Get appointment now</a>
     </section>
   )
 }
--- frontend/src/components/ImpressiveClean/ImpressiveClean.tsx
+++ frontend/src/components/ImpressiveClean/ImpressiveClean.tsx
@@ -9,7 +9,7 @@ const ImpressiveClean = () => {
         <p>Your office should reflect the professionalism and esteem of your company. CCC can deliver the impressive results your customers, clients, and employees expect when they walk into your business. Based in Birmingham, Alabama, we set the bar when it comes to commercial janitorial services that are ideal for office buildings, warehouses, plants, and factories. Simply put: nothing is left out, and no dirt is kept in!</p>
         <div className={styles.callUs}>
           <h3>Call us today at:</h3>
-          <a target='_blank' href="https://w.app/D9moOk">(205) 484-9624</a>
+          <a target='_blank' href="https://api.whatsapp.com/send?phone=5544988099490&text=Em%20que%20posso%20te%20ajudar?">(205) 484-9624</a>
         </div>
       </div>
       <div className={styles.impressiveImg}>
