3f8a3c2d78a47cab2d41d45d66fa932059b55a5c
Luiz Serão <luiz.serao@hotmail.com>
2024-05-31 09:14:35 -0300

    pequenos ajustes

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -26,7 +26,7 @@ const Home = () => {
       <QuoteEasy />
       <ImpressiveClean />
       <Contact />
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3602.1745618548216!2d-49.28564795001526!3d-25.465845959326856!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dce49d65c87e57%3A0xeb8cd6c6e843d0d0!2sR.%20Amazonas%20-%20Fazendinha%2FPort%C3%A3o%2C%20Curitiba%20-%20PR%2C%2080610-030!5e0!3m2!1spt-BR!2sbr!4v1717155237528!5m2!1spt-BR!2sbr" width="100%" height="450" style={{border: '0',marginTop: '4rem'}} loading="lazy"></iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3602.1745618548216!2d-49.28564795001526!3d-25.465845959326856!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dce49d65c87e57%3A0xeb8cd6c6e843d0d0!2sR.%20Amazonas%20-%20Fazendinha%2FPort%C3%A3o%2C%20Curitiba%20-%20PR%2C%2080610-030!5e0!3m2!1spt-BR!2sbr!4v1717155237528!5m2!1spt-BR!2sbr" width="100%" height="450" style={{border: '0',marginTop: '4rem',marginBottom: '0', display:'block'}} loading="lazy"></iframe>
     </div>
   )
 }
